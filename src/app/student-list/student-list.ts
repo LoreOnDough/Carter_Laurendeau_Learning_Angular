9c08cdf076dacd190467bd2b970a7061e7e7e8ce
“Carter <w0543536@myscc.ca>
2025-10-29 18:51:45 -0400

    added ngOnInit to student-list.ts

--- src/app/student-list/student-list.ts
+++ src/app/student-list/student-list.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from '../models/user';
 import {NgClass, NgForOf} from '@angular/common';
 import {StudentListItem} from '../student-list-item/studend-list-item';
+import {StudentService} from '../student-service';
 
 @Component({
   selector: 'app-student-list',
@@ -9,14 +10,18 @@ import {StudentListItem} from '../student-list-item/studend-list-item';
   templateUrl: './student-list.html',
   styleUrl: './student-list.css'
 })
-export class StudentList {
+export class StudentList implements OnInit{
   identifier:string[] = ['id', 'firstName', 'lastName', 'favouriteClass', 'isStudent'];
-  user1: User = {id:1, firstName: "Brendon", lastName:"Shaw", favouriteClass:"Java", isStudent: true};
-  user2: User = {id:2, firstName:"Nancy",lastName:"Miles",favouriteClass:"Python", isStudent: false};
-  user3: User = {id:3, firstName:"Katrina", lastName:"Blankenship", favouriteClass:"Javascript", isStudent: true};
-  user4: User = {id:4, firstName:"Owen", lastName:"Craig", favouriteClass:"HTML", isStudent: true};
-  user5: User = {id:5, firstName:"Brian", lastName:"Gallagher", favouriteClass:"Python", isStudent: false};
-  user6: User = {id:6, firstName:"Blake", lastName:"Bass", favouriteClass:"Java", isStudent: false};
-  userList: User[] = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6];
+  private userList: User[] | undefined = [];
+  constructor(private studentService:StudentService) {
+  }
+  ngOnInit(){
+    //This lifecycle hook is a good place to fetch and init our data
+    this.studentService.getStudents().subscribe({
+      next: (data: User[]) => this.userList = data,
+      error:err => console.error("Error fetching Students", err),
+      complete:() => console.log("Student data fetch complete!")
+    })
 
+  }
 }
