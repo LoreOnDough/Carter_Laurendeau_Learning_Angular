453e07f4d6ab071888869837232c15b79d3fb825
“Carter <w0543536@myscc.ca>
2025-10-29 18:45:49 -0400

    added getStudents to student-service.ts

--- src/app/student-service.ts
+++ src/app/student-service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import {User} from './models/user';
+import {userList} from './data/mock-content';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
-  
+  private students: User[] = userList;//Local copy of student data for CRUD Operations
+  constructor() { }
+  //Returns all students
+  getStudents(): Observable<User[]>{
+    return of(userList); //Return and observable that emits mock student data
+  }
 }
