8cacaab10fffda969c24a7a10970dc49b6b8bdef
“Carter <w0543536@myscc.ca>
2025-10-31 11:51:04 -0400

    added the buttons to delete

--- src/app/modify-student/modify-student.ts
+++ src/app/modify-student/modify-student.ts
@@ -12,7 +12,31 @@ import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} fr
 export class ModifyStudent {
   studentForm: FormGroup | undefined;
 
-  onSubmit() {
+  onSubmit(): void {
+    const student: User = this.studentForm.value;
 
+    // Check if we're updating an existing student
+    if (student.id) {
+      this.studentService.updateStudent(student);
+    } else {
+      // For adding a new student, generate a new ID
+      const newId = this.studentService.generateNewId(); // This method will create a new ID
+      student.id = newId;
+      this.studentService.addStudent(student);
+    }
+
+    this.router.navigate(['/students']);
+  }
+
+  onDelete(): void {
+    const id = this.studentForm.get('id')?.value;
+    if (id) {
+      this.studentService.deleteStudent(id);
+      this.router.navigate(['/students']);
+    }
+  }
+
+  navigateToStudentList(): void {
+    this.router.navigate(['/students']);
   }
 }
--- src/app/student-list-item/studend-list-item.ts
+++ src/app/student-list-item/studend-list-item.ts
@@ -8,21 +8,41 @@ import {userList} from '../data/mock-content';
   templateUrl: './studend-list-item.html',
   styleUrl: './studend-list-item.css'
 })
+
+
 export class StudentListItem {
+  constructor(item: User | undefined, currentIndex: number | undefined, userList: User[]) {
+    this.item = item;
+    this.currentIndex = currentIndex;
+    this.userList = userList;
+  }
  @Input() item?: User | undefined;
-  currentIndex: number;
+  currentIndex: number | undefined;
 
-  goBackward() {
-
+  //function to go back to student-list view
+  goBack(): void {
+    this.router.navigate(['/students']);
   }
 
-  goForward() {
-
+//function to move foward through array with overflow protection
+  goForward(): void {
+    // @ts-ignore
+    if (this.currentIndex < this.userList.length - 1) {
+      // @ts-ignore
+      this.currentIndex++;
+      // @ts-ignore
+      this.router.navigate(['/students', this.userList[this.currentIndex].id]);
+    }
   }
-
-  goBack() {
-
+//function to move backward through array with overflow protection
+  goBackward(): void {
+    // @ts-ignore
+    if (this.currentIndex > 0) {
+      // @ts-ignore
+      this.currentIndex--;
+      // @ts-ignore
+      this.router.navigate(['/students', this.userList[this.currentIndex].id]);
+    }
   }
-
   protected readonly userList = userList;
 }
