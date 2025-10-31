96c2025118778ea5910d22d1d7aa562559b53ee5
“Carter <w0543536@myscc.ca>
2025-10-31 11:11:05 -0400

    added routes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {Routes} from '@angular/router';
+import {StudentListItem} from './app/student-list-item/studend-list-item';
+
+const routes: Routes = [
+  {path:'', redirectTo: '/students', pathMatch: 'full'}, //default route
+  { path: 'students', component: StudentListItem },
+  { path: 'students/:id', component: StudentListItem },
+
+];
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
