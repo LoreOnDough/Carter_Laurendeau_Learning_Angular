27e39378fce3575ef37c460b3456994213e8922c
“Carter <w0543536@myscc.ca>
2025-11-13 18:25:50 -0500

    added lazy loading

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,20 @@ import {Routes} from '@angular/router';
 import {StudendListItem} from './app/student-list-item/studend-list-item';
 import {PageNotFound} from './app/page-not-found/page-not-found';
 import {ModifyStudent} from './app/modify-student/modify-student';
+import {StudentList} from './app/student-list/student-list';
 
 const routes: Routes = [
-  {path:'', redirectTo: '/students', pathMatch: 'full'}, //default route
-  { path: 'students', component: StudendListItem },
-  { path: 'students/:id', component: StudendListItem },
-  {path:'modify-student', component: ModifyStudent},
-  {path: '**', component: PageNotFound}//Wildcard route for a 404 page
-
+  { path: '', redirectTo: '/students', pathMatch: 'full' }, //Default route
+  { path: 'students', component: StudentList },     //eagerly loaded
+  { path: 'students/:id',
+    loadComponent: () =>
+      import('./app/student-list-item/studend-list-item').then(m => m.StudendListItem) }, //Lazy Loaded
+  { path: 'modify-student',
+    loadComponent: () =>
+      import('./app/modify-student/modify-student').then(m => m.ModifyStudent) },
+  { path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found').then(m => m.PageNotFound) },
 ];
-
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
