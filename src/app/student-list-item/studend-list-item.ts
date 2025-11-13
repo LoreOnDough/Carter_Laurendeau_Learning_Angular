955ba4505c60eb95b6dd77135e30ebe7894f110d
“Carter <w0543536@myscc.ca>
2025-11-13 18:12:49 -0500

    added the pipes to studend-list-item.ts

--- src/app/student-list-item/studend-list-item.ts
+++ src/app/student-list-item/studend-list-item.ts
@@ -1,10 +1,15 @@
 import {Component, Input, input} from '@angular/core';
 import {User} from '../models/user';
 import {userList} from '../data/mock-content';
+import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-student-list-item',
-  imports: [],
+  imports: [
+    UpperCasePipe,
+    LowerCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './studend-list-item.html',
   styleUrl: './studend-list-item.css',
   standalone:true
