f65374e8136544bfa1cd25a8128a643a9ed5b53f
“Carter <w0543536@myscc.ca>
2025-11-26 18:50:13 -0500

    finished the project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,23 @@
 import {Component, OnInit, signal} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {User} from './models/user';
 import {NgForOf, NgSwitch, NgSwitchCase} from '@angular/common';
 import {StudentList} from './student-list/student-list';
 import {StudendListItem} from './student-list-item/studend-list-item';
 import {StudentService} from './student-service';
 import {Observable} from 'rxjs';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   imports: [
-    StudentList
+    StudentList,
+    MatToolbar,
+    MatButton,
+    RouterLink,
+    RouterLinkActive
   ],
   styleUrl: './app.css'
 })
--- src/app/modify-student/modify-student.ts
+++ src/app/modify-student/modify-student.ts
@@ -10,6 +10,7 @@ import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatCheckbox} from '@angular/material/checkbox';
 import {MatButton} from '@angular/material/button';
+import {MatTooltip} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-modify-student',
@@ -21,7 +22,8 @@ import {MatButton} from '@angular/material/button';
     MatLabel,
     MatInput,
     MatCheckbox,
-    MatButton
+    MatButton,
+    MatTooltip
   ],
   templateUrl: './modify-student.html',
   styleUrl: './modify-student.css'
